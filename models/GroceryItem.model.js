960ef02d62e3f143bc0667c36b294b6f003e9577
Alia Dossani <dossanialia1408@gmail.com>
2024-01-31 10:55:54 +0100

    added default image to groceryItem

--- models/GroceryItem.model.js
+++ models/GroceryItem.model.js
@@ -8,8 +8,8 @@ const groceryItemSchema = new Schema(
       trim: true,
     },
     image:{
-      type:String,
-      default:"https://www.freepik.com/free-photo/top-view-fresh-vegetables-arrangement_22632474.htm#query=grocery&position=7&from_view=keyword&track=sph&uuid=8243c70f-4c68-4b2a-98eb-a362e010d867",
+      type: String,
+      default:"https://img.freepik.com/free-photo/top-view-fresh-vegetables-arrangement_23-2149271094.jpg?w=900&t=st=1706691719~exp=1706692319~hmac=aa12dce3ae0d9b6cf827cc6c9540cc1d90211bd91bd0d201f4f1770b1f3fdf5f",
     },
     quantity: {
       type: String,
